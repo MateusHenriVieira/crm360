6b26517615868a12d517d774f1ac8fde0cb7f8a0
HenriqueFerreira7 <henriqueferreira1028@gmail.com>
2025-09-23 02:34:38 -0300

    added login

--- backend/app/models/request_schemas.py
+++ backend/app/models/request_schemas.py
@@ -7,3 +7,10 @@ class NewAccountSchema(BaseModel):
     name: str
     email: str
     password: str
+    class Config:
+        from_attributes = True
+class LoginSchema(BaseModel):
+    email: str
+    password: str
+    class Config:
+        from_attributes = True
