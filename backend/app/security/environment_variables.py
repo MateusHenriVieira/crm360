fccec0526bf3e8a95266a331ea1bd2e9327902d1
HenriqueFerreira7 <henriqueferreira1028@gmail.com>
2025-09-24 03:15:22 -0300

    cleaning main

--- backend/app/security/environment_variables.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-import os
-load_dotenv()
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
-ADMIN_NAME = os.getenv("ADMIN_NAME")
-ADMIN_EMAIL = os.getenv("ADMIN_EMAIL")
-ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
--- backend/app/security/password_hash.py
+++
@@ -1,7 +0,0 @@
-# Todo o modulo security deverá ser binarizado posteriormente ao final do projeto
-
-
-from passlib.context import CryptContext
-
-
-encrypter = CryptContext(["bcrypt"])
